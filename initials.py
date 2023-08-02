eac6fea1c114974bcedbcfda222ebfd2a46261dd
SowahKwame <141031036+SowahKwame@users.noreply.github.com>
2023-08-02 22:26:44 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,25 +1,14 @@
-# -*- coding: utf-8 -*-
-#File:initials.py
-#Description:Print out my initials
-#Assignment Number:1
-#Name:Kelvin Sowah Kwame Dovlo
-#STUDENT ID:3231230036
-#Email:3231230036@gctu.edu.gh
-#Grader: Augustus
-#
-# On my honor, Kelvin Sowah Dovlo, this programming assignment is my own work
-# and i have not provided this code to any other student.
+import os.path
+import sys
+from initials import main
 
-print()
-print("...KDS");
-print()
-print("...KKKk.....KKK........DDDDDDDDD..............SSSSSSSSS.....")
-print("...KKKK....KKKK........DDDDDDDDDD............SSSSSSSSSS.....")
-print("...KKKK...KKKKK........DD.......DDD.........SSSS............")
-print("...KKKK..K.............DD........DD.........SSSS............")
-print("...KKKK.K..............DD........DD...........SSSSSS........")
-print("...KKKKKK..............DD........DD.............SSSSSS......")
-print("...KKKK..K.............DD........DD.................SSSS....")
-print("...KKKK...KKKKK........DD.......DDD.................SSSS....")
-print("...KKKK...KKKKK...**...DDDDDDDDDD.....**...SSSSSSSSSSSS..,**")
-print("...KKK.....KKKK...**...DDDDDDDD.......**...SSSSSSSSSSS....**")
+def initials_test():
+  try:
+    exists=os.path.exists("initials.py")
+    assert exists == true
+  except:
+    sys.exit()
+
+  
+  main()
+
